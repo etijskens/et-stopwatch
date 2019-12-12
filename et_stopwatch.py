e4b8d239f67dd43f1baeac306e4ba6139f1a7e4d
Engelbert Tijskens <engelbert.tijskens@uantwerpen.be>
2019-12-12 16:42:02 +0100

    stopwatch

--- et_stopwatch.py
+++ et_stopwatch.py
@@ -1,20 +1,83 @@
-# -*- coding: utf-8 -*-
 """
-Package et_stopwatch
-=======================================
-
-A 'hello world' example.
+Module et_stopwatch
+===================
+A context manager class for timing a piece of code.
 """
-__version__ = "0.0.0"
 
+from timeit import default_timer as timer
 
-def hello(who='world'):
-    """'Hello world' method.
+class Stopwatch:
+    """Context manager class for timing a code fragment.
+    
+    Usage::
+    
+       with Stopwatch('This took me') as sw:
+           for i in range(3):
+               sleep(1)
+               print(i,sw.timelapse(),'s)
+               
+        0 1.004215 s
+        1 1.003996 s
+        2 1.001949 s
+        This took me 3.010238 s
 
-    :param str who: whom to say hello to
-    :returns: a string
+    :param str comment: text in front of the total time. If None nothing is printed. Default is empty str,
+    :param int ndigits: number of digits after the decimal sign.
     """
-    result = "Hello " + who
-    return result
+    def __init__(self,message='',ndigits=6):
+        self.started = -1.0
+        self.stopped = -1.0
+        self.message = message
+        self.ndigits = ndigits
 
-# eof
+    
+    def __enter__(self):
+        self.start()
+        return self
+
+
+    def __exit__(self, exception_type, exception_value, tb):
+        self.stop()
+        if not self.message is None:
+            print(self)
+
+    
+    #@property
+    # DO NOT use the @property decorator for functions that change the state!
+    def timelapse(self):
+        """Return number of seconds (float) since last call to timelapse (or to start if 
+        called for the first time).
+        """
+        now = timer()
+        seconds = round(now - self.stopped,self.ndigits)
+        self.stopped = now
+        return seconds
+
+
+    @property
+    def time(self):
+        """Return number of seconds between starting and stopping the stopwatch (for the last time)"""
+        return round(self.stopped-self.started,self.ndigits)
+
+
+    def start(self):
+        """Start or restart this :py:class:`Stopwatch` object."""
+        self.started = timer()
+        self.stopped = self.started
+    
+
+    def stop(self):
+        """Stop the :py:class:`Stopwatch` object and return the number of seconds since it was started."""
+        self.stopped = timer()
+        return self.time
+    
+    
+    def __repr__(self):
+        if not self.message is None:
+            if not self.message.endswith(' '):
+                self.message += ' '
+            return self.message+'{} s'.format(self.time)
+        else:
+            return              '{} s'.format(self.time)
+
+#eof
--- tests/test_et_stopwatch.py
+++ tests/test_et_stopwatch.py
@@ -2,21 +2,59 @@
 
 """Tests for et_stopwatch package."""
 
-import et_stopwatch
+from time import sleep
+from contextlib import redirect_stdout
+import io
+from et_stopwatch import Stopwatch
+
+def test_1():
+    print("\nuse case 1: time a piece of code.")
+    f = io.StringIO()
+    with redirect_stdout(f):
+        with Stopwatch():
+            # piece of code to be timed
+            sleep(1)
+    s = f.getvalue()
+    s0 = s.split(' ')[1]
+    t = float(s0)
+    assert t >= 1
+    assert t < 1.01
 
 
-def test_hello_noargs():
-    """Test for et_stopwatch.hello()."""
-    s = et_stopwatch.hello()
-    assert s=="Hello world"
+def test_2():
+    print("\nuse case 2: time a piece of code, with a message.")
+    f = io.StringIO()
+    with redirect_stdout(f):
+        with Stopwatch("time 'sleep(1)': "):
+            # piece of code to be timed
+            sleep(1)
+    t = float(f.getvalue().split(' ')[2])
+    assert t >= 1
+    assert t < 1.01
 
+def test_3():
+    print("\nuse case 3: time a piece of code, listing intermediate steps, with a message, rounding times at 3 digits.")
+    f = io.StringIO()
+    with redirect_stdout(f):
+        with Stopwatch("time 5 times 'sleep(1)': ",ndigits=3) as tmr:
+            for i in range(5):
+                sleep(1)
+                t = tmr.timelapse()
+                print(i,t)
+                assert t >= 1
+                assert t < 1.01
 
-def test_hello_me():
-    """Test for et_stopwatch.hello('me')."""
-    s = et_stopwatch.hello('me')
-    assert s=="Hello me"
-    
-    
+        print(tmr.time)
+        assert tmr.time >= 5
+        assert tmr.time < 5.05
+    s = f.getvalue()
+    L5 = s.split('\n')[5]
+    t = float(L5.split(' ')[4])
+    assert t >= 5
+    assert t < 5.05
+    print(t)
+    print(s)
+
 # ==============================================================================
 # The code below is for debugging a particular test in eclipse/pydev.
 # (otherwise all tests are normally run with pytest)
@@ -24,7 +62,7 @@ def test_hello_me():
 # that the source directory is on the path
 # ==============================================================================
 if __name__ == "__main__":
-    the_test_you_want_to_debug = test_hello_noargs
+    the_test_you_want_to_debug = test_1
 
     print("__main__ running", the_test_you_want_to_debug)
     the_test_you_want_to_debug()
