ffe961a7268a220386eaac4b27a170f00d60091a
Engelbert Tijskens <engelbert.tijskens@uantwerpen.be>
2020-01-13 09:24:59 +0100

    improved
    - computes statistics if called more than once
    - using ideas from https://realpython.com/python-timer/#a-python-timer-decorator
    -version bump

--- et_stopwatch.py
+++ et_stopwatch.py
@@ -1,36 +1,76 @@
 """
 Module et_stopwatch
 ===================
-A context manager class for timing a piece of code.
+A class for timing a piece of code.
 """
+__version__ = "0.2.1"
 
 from timeit import default_timer as timer
+from sys import float_info
+import functools
+from math import sqrt
 
-__version__ = "0.2.0"
 
 class Stopwatch:
-    """Context manager class for timing a code fragment.
+    """Class for timing code.
+
+    Use as class::
+
+        stopwatch = Stopwatch()
+        sleep(1)
+        stopwatch.stop()
+        print(stopwatch)
+
+        >>> stopwatch : 1.003744 s
+
+    Use as context manager::
+
+        with Stopwatch('This took') as sw:
+            for i in range(3):
+                sleep(1)
+                print(i, sw.stop())
+
+        0 1.004194
+        1 1.003886
+        2 1.000814
+        This took :
+            total  : 3.008894 s
+            minimum: 1.000814 s
+            maximum: 1.004194 s
+            mean   : 1.002965 s
+            stddev : 0.001526 s
+            count  : 3
+
+    Use as decorator::
+
+        @Stopwatch(name="say_hi_and_sleep_two_seconds", ndigits=3)
+        def say_hi_and_sleep_two_seconds():
+            print("hi")
+            sleep(2)
+
+        say_hi_and_sleep_two_seconds()
+
+        hi
+        say_hi_and_sleep_two_seconds : 2.003 s
+
+    Constructor parameters:
     
-    Usage::
-
-       with Stopwatch('This took me') as sw:
-           for i in range(3):
-               sleep(1)
-               print(i,sw.timelapse(),'s)
-               
-        0 1.004215 s
-        1 1.003996 s
-        2 1.001949 s
-        This took me 3.010238 s
-
-    :param str comment: text in front of the total time. If None nothing is printed. Default is empty str,
+    :param str name: text in front of the total time. If None nothing is printed. Default is empty str,
     :param int ndigits: number of printed decimal digits in printed timings.
+
+    Inspiration taken from `RealPython Python Timer Functions: Three Ways to Monitor Your Code <https://realpython.com/python-timer/#a-python-timer-decorator>`_
     """
-    def __init__(self,message='',ndigits=6):
+    def __init__(self,name='Stopwatch',ndigits=6):
         self.started = -1.0
         self.stopped = -1.0
-        self.message = message
+        self.max = 0.
+        self.min = float_info.max
+        self.count = 0
+        self.sum = 0.
+        self.ssq = 0.
+        self.name = name
         self.ndigits = ndigits
+        self.start()
 
     
     def __enter__(self):
@@ -39,47 +79,103 @@ class Stopwatch:
 
 
     def __exit__(self, exception_type, exception_value, tb):
-        self.stop()
-        if not self.message is None:
-            print(self)
+        if self.count == 0:
+            self.stop()
+        print(self)
 
     
-    #@property
-    # DO NOT use the @property decorator for functions that change the state!
-    def timelapse(self):
-        """Return number of seconds (float) since last call to timelapse (or to start if 
-        called for the first time).
-        """
-        now = timer()
-        seconds = round(now - self.stopped,self.ndigits)
-        self.stopped = now
-        return seconds
-
-
-    @property
-    def time(self):
-        """Return number of seconds between starting and stopping the :py:class:`Stopwatch` (for the last time)"""
-        return round(self.stopped-self.started,self.ndigits)
-
-
     def start(self):
         """Start or restart this :py:class:`Stopwatch` object."""
         self.started = timer()
         self.stopped = self.started
-    
 
+
+    #@property    # NEVER use the @property decorator for functions that change the state!
     def stop(self):
-        """Stop the :py:class:`Stopwatch` object and return the number of seconds since it was started."""
+        """Stop the stopwatch and return number of seconds (float) since the latest call to stop or start.
+        """
         self.stopped = timer()
-        return self.time
-    
-    
-    def __repr__(self):
-        if not self.message is None:
-            if not self.message.endswith(' '):
-                self.message += ' '
-            return self.message+'{} s'.format(self.time)
-        else:
-            return              '{} s'.format(self.time)
+        self.count += 1
+        t = self.time
+        if t < self.min:
+            self.min = t
+        if t > self.max:
+            self.max = t
+        self.sum += t
+        self.ssq += t*t
+        self.start()
+        self.last = t
+        return t
 
+
+    @property
+    def time(self):
+        """Return number of seconds between the latest start and stop the :py:class:`Stopwatch`."""
+        return round(self.stopped-self.started, self.ndigits)
+
+
+    def statistics(self):
+        """Compute mean and standard deviation."""
+        self.mean = round(self.sum / self.count, self.ndigits)
+        self.stddev = round(sqrt( (self.ssq + self.mean * (self.count * self.mean - 2. * self.sum)) / (self.count) ), self.ndigits)
+        return self.mean, self.stddev
+
+
+    def __repr__(self):
+        """
+        Print the objects name and total time. If stop was called more than once also statistics are printed
+        (min, max, mean, stddev, count).
+        """
+        message = self.name + " : "
+        if self.count == 1:
+            message += "{} s".format(self.last)
+        else:
+            mean, stddev = self.statistics()
+            message += "\n    total  : {} s"\
+                       "\n    minimum: {} s"\
+                       "\n    maximum: {} s"\
+                       "\n    mean   : {} s"\
+                       "\n    stddev : {} s"\
+                       "\n    count  : {}"
+            message = message.format(self.sum, self.min, self.max, mean, stddev, self.count)
+        return message
+    
+
+    def __call__(self, func):
+        """Support using StopWatch as a decorator"""
+        @functools.wraps(func)
+        def wrapper_stopwatch(*args, **kwargs):
+            with self:
+                return func(*args, **kwargs)
+
+        return wrapper_stopwatch
+
+if __name__ == "__main__":
+    """some use cases"""
+    from time import sleep
+    print("# Use as class:")
+    stopwatch = Stopwatch()
+    sleep(1)
+    stopwatch.stop()
+    print(stopwatch)
+
+    print()
+
+    print("# Use as context manager:")
+    with Stopwatch('This took') as sw:
+        for i in range(3):
+            sleep(1)
+            print(i, sw.stop())
+
+    print()
+    print("# Use as decorator:")
+
+    @Stopwatch(name="say_hi_and_sleep_two_seconds", ndigits=3)
+    def say_hi_and_sleep_two_seconds():
+        print("hi")
+        sleep(2)
+
+    say_hi_and_sleep_two_seconds()
+
+    print("-*# done #*-")
 #eof
--- tests/test_et_stopwatch.py
+++ tests/test_et_stopwatch.py
@@ -5,9 +5,38 @@
 from time import sleep
 from contextlib import redirect_stdout
 import io
+import numpy as np
+import math
 from et_stopwatch import Stopwatch
 
-def test_1():
+def test_class_1():
+    ta = []
+    stopwatch = Stopwatch(name='my timer')
+    sleep(1)
+    t = stopwatch.stop()
+    print(t)
+    assert t >= 1
+    assert t < 1.01
+    assert stopwatch.count == 1
+    print(stopwatch)
+    ta.append(t)
+
+    sleep(1)
+    t = stopwatch.stop()
+    print(t)
+    assert t >= 1
+    assert t < 1.01
+    assert stopwatch.count == 2
+    ta.append(t)
+    print(stopwatch)
+
+    mean = np.mean(ta)
+    stddev = np.std(ta)
+    # print(mean,stddev)
+    assert math.isclose(mean, stopwatch.mean, abs_tol=1.0e-6)
+    assert math.isclose(stddev, stopwatch.stddev, abs_tol=1.0e-6)
+
+def test_context_manager_1():
     print("\nuse case 1: time a piece of code.")
     f = io.StringIO()
     with redirect_stdout(f):
@@ -15,54 +44,58 @@ def test_1():
             # piece of code to be timed
             sleep(1)
     s = f.getvalue()
-    s0 = s.split(' ')[1]
+    s = s.split(' ')
+    s0 = s[2]
     t = float(s0)
     assert t >= 1
     assert t < 1.01
 
 
-def test_2():
+def test_context_manager_2():
     print("\nuse case 2: time a piece of code, with a message.")
     f = io.StringIO()
     with redirect_stdout(f):
-        with Stopwatch("time 'sleep(1)': "):
+        with Stopwatch("time 'sleep(1)'"):
             # piece of code to be timed
             sleep(1)
-    t = float(f.getvalue().split(' ')[2])
+    s = f.getvalue()
+    print(s)
+    t = float(s.split(' ')[3])
     assert t >= 1
     assert t < 1.01
 
-def test_3():
+def test_context_manager_3():
     print("\nuse case 3: time a piece of code, listing intermediate steps, with a message, rounding times at 3 digits.")
     f = io.StringIO()
     with redirect_stdout(f):
         with Stopwatch("time 5 times 'sleep(1)': ",ndigits=3) as tmr:
             for i in range(5):
                 sleep(1)
-                t = tmr.timelapse()
+                t = tmr.stop()
                 print(i,t)
                 assert t >= 1
                 assert t < 1.01
 
-        print(tmr.time)
-        assert tmr.time >= 5
-        assert tmr.time < 5.05
-    s = f.getvalue()
-    L5 = s.split('\n')[5]
-    t = float(L5.split(' ')[4])
-    assert t >= 5
-    assert t < 5.05
-    print(t)
-    print(s)
+        print(tmr)
+        assert tmr.sum >= 5
+        assert tmr.sum < 5.05
+        assert tmr.count == 5
+
+
+def test_decorator_1():
+    @Stopwatch('my timer')
+    def mysleep():
+        print("hi")
+        sleep(1)
+    mysleep()
+
 
 # ==============================================================================
-# The code below is for debugging a particular test in eclipse/pydev.
+# The code below is for debugging a particular test.
 # (otherwise all tests are normally run with pytest)
-# Make sure that you run this code with the project directory as CWD, and
-# that the source directory is on the path
 # ==============================================================================
 if __name__ == "__main__":
-    the_test_you_want_to_debug = test_3
+    the_test_you_want_to_debug = test_decorator_1
 
     print("__main__ running", the_test_you_want_to_debug)
     the_test_you_want_to_debug()
